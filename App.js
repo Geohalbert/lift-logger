98102d76e591ec980e5069c4efac7eb57bb24726
Hal Peters <geohalbert@gmail.com>
2020-08-04 00:59:39 -0500

    reorganize app

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunkMiddleware from "redux-thunk";
 
-import AppNavigator from "./navigation/AppNavigator";
-import reducer from "./reducers";
+import AppNavigator from "./src/navigation/AppNavigator";
+import reducer from "./src/reducers";
 
 const middleware = applyMiddleware(thunkMiddleware);
 const store = createStore(reducer, middleware);
