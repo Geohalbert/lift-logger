bc890d7aa01d8f4d23386aad8a276a29a350ced4
Hal Peters <geohalbert@gmail.com>
2020-08-07 14:50:28 -0500

    setting up database structure

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const onUserCreated = require("./onUserCreated.js");
+const onUserWorkoutCreated = require("./onUserWorkoutCreated.js");
 
 /* Functions */
 exports.onUserCreated = onUserCreated;
+exports.onUserWorkoutCreated = onUserWorkoutCreated;
 
 /* HTTP Functions */
 
