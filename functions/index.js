25b68e9ae57b9e1c7ca00f48548fb3d164c8f6cb
Hal Peters <geohalbert@gmail.com>
2020-08-17 06:06:52 -0500

    changed database to realtime

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const onUserCreated = require("./onUserCreated.js");
-const onUserWorkoutCreated = require("./onUserWorkoutCreated.js");
 
 /* Functions */
 exports.onUserCreated = onUserCreated;
-exports.onUserWorkoutCreated = onUserWorkoutCreated;
 
 /* HTTP Functions */
 
--- functions/onUserCreated.js
+++ functions/onUserCreated.js
@@ -1,17 +1,22 @@
 const functions = require("firebase-functions");
 const admin = require("./admin");
 
-const db = admin.firestore();
+const db = admin.database();
 
-module.exports = functions.auth.user().onCreate(user => {
-  const userCollection = db.collection("users");
-  var email = user.email;
+module.exports = functions.auth.user().onCreate(async user => {
+  const email = user.email;
   const stamp = new Date().getTime();
-  return userCollection.doc(user.uid).set({
-    email: email && email.toLowerCase ? email.toLowerCase() : email,
-    uid: user.uid,
-    createdAt: stamp,
-    updatedAt: stamp,
-    userWorkouts: []
-  });
+  try {
+    const snapshot = await db.ref("users/" + user.uid).set({
+      email: email && email.toLowerCase ? email.toLowerCase() : email,
+      uid: user.uid,
+      createdAt: stamp,
+      updatedAt: stamp,
+      userWorkouts: []
+    });
+    return snapshot;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 });
