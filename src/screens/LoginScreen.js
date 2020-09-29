0446aea2a0d0eb50518b4da899e09927933b800e
Hal Peters <geohalbert@gmail.com>
2020-09-29 17:47:04 -0500

    LoginScreen is functional component

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -7,24 +7,74 @@ import {
   TextInput,
   ActivityIndicator
 } from "react-native";
-import { login } from "../redux/actions/user";
+import {
+  login,
+  setUser,
+  updateEmail,
+  updatePassword,
+  getUser,
+  register
+} from "../redux/actions/user";
 
 import colors from "../assets/colors";
 import CustomAction from "../components/CustomAction";
+import InputField from "../components/InputField";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 
-export default function LoginScreen(props) {
+export default function LoginScreen({
+  login,
+  setUser,
+  updateEmail,
+  updatePassword,
+  getUser,
+  register
+}) {
   const dispatch = useDispatch();
 
-  const [password, setPassword] = useState("");
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isEmailInvalid, onChangeEmailError] = useState(false);
   const [isPasswordInvalid, onChangePasswordError] = useState(false);
   const [isLoginInvalid, onChangeLoginError] = useState(false);
   const [shouldDisplayMessage, onChangeDisplayMessage] = useState(true);
-  const [email, setEmail] = useState("");
+
+  onUpdateEmail = text => {
+    onChangeEmail(text);
+
+    // clear email error
+    if (isEmailInvalid) {
+      onChangeEmailError(false);
+    }
+    // clear login error
+    if (isLoginInvalid) {
+      onChangeLoginError(false);
+    }
+    // clear success message
+    if (shouldDisplayMessage) {
+      onChangeDisplayMessage(false);
+    }
+  };
+
+  onUpdatePassword = text => {
+    onChangePassword(text);
+
+    // a-z A-Z 0-9 . - _ $ * ( ) # @ ! % / ^
+    const passwordIsValid = /^[\w.-_$*()#@!%/]+$/.test(text);
+
+    onChangePasswordError(!passwordIsValid);
+
+    // clear login error
+    if (isLoginInvalid) {
+      onChangeLoginError(false);
+    }
+    // clear success message
+    if (shouldDisplayMessage) {
+      onChangeDisplayMessage(false);
+    }
+  };
 
   onSignIn = async () => {
     if (email && password) {
@@ -42,6 +92,9 @@ export default function LoginScreen(props) {
         switch (error.code) {
           case "auth/user-not-found":
             alert("A user with that email does not exist. Try signing Up");
+            break;
+          case "auth/invalid-password":
+            alert("Please enter at least six characters");
             break;
           case "auth/invalid-email":
             alert("Please enter an email address");
@@ -62,10 +115,22 @@ export default function LoginScreen(props) {
         }
       } catch (error) {
         setIsLoading(false);
-        if (error.code == "auth/email-already-in-use") {
-          alert("User already exists, try logging in");
+        switch (error.code) {
+          case "auth/user-not-found":
+            alert("A user with that email does not exist. Try signing Up");
+            break;
+          case "auth/invalid-password":
+            alert("Please enter at least six characters");
+            break;
+          case "auth/email-already-in-use":
+            alert("User already exists, try logging in");
+            break;
+          case "auth/invalid-email":
+            alert("Please enter an email address");
+            break;
+          default:
+            alert("Please enter a valid email and password");
         }
-        console.log(error);
       }
     } else {
       alert("Please enter email and password");
@@ -89,30 +154,39 @@ export default function LoginScreen(props) {
           <ActivityIndicator size="large" color={colors.logoColor} />
         </View>
       ) : null}
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <TextInput
-          style={styles.textInput}
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <InputField
+          isLogin
+          onChangeText={text => onUpdateEmail(text)}
+          label="email"
+          autoCompleteType="email"
+          error={isEmailInvalid}
+          keyboardType="email-address"
+          errorMessage={"Please enter a valid email address."}
+          value={email}
           placeholder={"abc@example.com"}
           placeholderTextColor={colors.bgTextInputDark}
-          keyboardType="email-address"
-          onChangeText={email => setEmail(email)}
         />
-        <TextInput
-          style={styles.textInput}
+        <InputField
+          isLogin
+          onChangeText={text => onUpdatePassword(text)}
+          label="password"
+          secureTextEntry
+          error={isPasswordInvalid}
+          errorMessage={"Please enter a valid password."}
+          value={password}
           placeholder="enter password"
           placeholderTextColor={colors.bgTextInputDark}
-          secureTextEntry
-          onChangeText={password => setPassword(password)}
         />
         <View style={{ alignItems: "center" }}>
           <CustomAction
-            onPress={() => this.onSignIn()}
+            onPress={() => onSignIn()}
             style={[styles.loginButtons, { borderColor: colors.bgPrimary }]}
           >
             <Text style={{ color: "white" }}>Login</Text>
           </CustomAction>
           <CustomAction
-            onPress={() => this.onSignUp()}
+            onPress={() => onSignUp()}
             style={[styles.loginButtons, { borderColor: colors.bgError }]}
           >
             <Text style={{ color: "white" }}>Sign Up</Text>
